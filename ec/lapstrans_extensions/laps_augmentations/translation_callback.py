9fb97c9900e39e3466c84ae99eab807a7a4180db
gosha <Goshagosha>
2022-08-28 13:59:21 +0200

    Removed type declaration with a Frontier class from dreamcoder
    python3.7 does not respect "TYPE_CHECKING" constant from types library,
    and raises Exception on code execution
    	modified:   ec/lapstrans_extensions/laps_augmentations/translation_callback.py

--- ec/lapstrans_extensions/laps_augmentations/translation_callback.py
+++ ec/lapstrans_extensions/laps_augmentations/translation_callback.py
@@ -1,12 +1,10 @@
 import json
-from typing import List, TYPE_CHECKING
-if TYPE_CHECKING:
-    from ec.dreamcoder.frontier import Frontier
+from typing import List
 
 WRITE_TO = "lapstrans_extensions/working_dir/translation.json"
 
 
-def write_translations(frontiers: List[Frontier]) -> None:
+def write_translations(frontiers: List) -> None:
     """When using LAPS to translate python code, this function will be called after one pass in dreamcoder.py, write down the translation results into .json file, and kill the execution.
 
     Args:
