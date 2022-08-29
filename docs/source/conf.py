56953b5269ee2faddf37a6fa4ab87e98cb2443be
Szmajasz <56126227+Szmajasz@users.noreply.github.com>
2022-08-29 03:28:04 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,6 +54,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+
+import sphinx_book_theme
 html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
